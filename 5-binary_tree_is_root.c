5b6c2ffdf2505131c9b64e89744f07c7e4773c0e
muyenzi10 <muyenzija10@gmail.com>
2024-01-31 07:40:32 -0800

    updated

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - checks if aewqeweq
+ *
+ * @node: poinewkmkw kwekq
+ * Return: 1 if node is a root, otherwise 0
+ * If node is NULL, return 0
+ */
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node && !node->parent)
+		return (1);
+	return (0);
+}
---
+++ 9-binary_tree_height.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - medsfksd ksdsdkb kfbbks
+ *
+ * @tree: pointer sffd sdfd
+ * Return: 0
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t h = 0, hr = 0;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left)
+		h = 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		hr = 1 + binary_tree_height(tree->right);
+
+	if (h > hrg)
+		return (h);
+	return (hr);
+}
