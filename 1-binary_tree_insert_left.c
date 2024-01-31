f873d339239ace095cc99daad7b59b65f9131503
muyenzi10 <muyenzija10@gmail.com>
2024-01-31 02:47:00 -0800

    up

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - inserts a node as the left-child of another node
+ * binary_tree_insert_left - inserts a n
  *
- * @parent: pointer to the node to insert the left-child in
- * @value: value to store in the new node
- * Return: pointer to the created new node. NULL on failure
+ * @parent: pointer to the node to indadadsa
+ * @value: value to store in taadd
+ * Return: pointer to the created neasxax
  */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
